d701861a61b1b7af146e6a34a662e182384166b2
Spyral96 <ddigangi75@gmail.com>
2024-01-30 15:28:33 -0500

    add the Create Canvas Button the ability to delete prevoius canvases

--- script.js
+++ script.js
@@ -14,16 +14,30 @@ function getCanvasSize()
 
 
 let pixelContainer =  document.querySelector(".pixelContainer");
+
+rotatingCanvasNum = [];
+
+let noCanvas = true;
+
 let canvasNum = createCanvasButton.addEventListener('click',function()
 {
     canvasNum = getCanvasSize();
     console.log(canvasNum);
+    
+    if (noCanvas=== false)
+    {
+        removeCanvas();
+    }
+
+    
+
 
     //Generating Canvas that is made from the user selected number. the number is squared to make said canvas
     //and each title (div)has an id num in order of its position
 
     for (let i = 0; i <canvasNum; i++)
     {
+        console.log(canvasNum);
         let row = document.createElement('div');
         //to not have row start a zero
         row.id = `row${i +1}`;
@@ -33,6 +47,8 @@ let canvasNum = createCanvasButton.addEventListener('click',function()
         pixelContainer.appendChild(row);
         
         
+
+
         for (let l = 1; l <= canvasNum; l++)
         {
             let column = document.createElement('div');
@@ -51,12 +67,90 @@ let canvasNum = createCanvasButton.addEventListener('click',function()
                 column.id = `pixel${l + i * canvasNum}`;
             }
 
+
+
             
 
         }
         
-
+        
+        
+        
+        
     }
+
+    noCanvas = false;
+    if (rotatingCanvasNum.length <3)
+    {
+    rotatingCanvasNum.unshift(canvasNum);
+    console.log(`list${rotatingCanvasNum}`)
+    }
+
+    else
+    {
+        
+        rotatingCanvasNum.unshift(canvasNum);
+        console.log(`list${rotatingCanvasNum}`)
+        rotatingCanvasNum.pop();
+    }
+    
+
 });
 
-console.log(1 + 2 *7);
+
+function removeCanvas()
+{
+    let lastCanvasNum = rotatingCanvasNum[0];
+    console.log(">")
+    console.log(lastCanvasNum);
+    console.log(canvasNum + "current");
+    pixelsToDelete = document.querySelector("#pixel1");
+    rowsToDelete = document.querySelector("#row1");
+
+    
+    
+     
+
+     for (i = 1; i <=lastCanvasNum;i++)
+     {
+        rowsToDelete = document.querySelector(`#row${i}`);
+        console.log(`row${i} starting`);
+
+        for (l = 1;l <=lastCanvasNum;l++)
+        {   
+            if (i === 1)
+            {
+                pixelsToDelete = document.querySelector(`#pixel${l}`);
+                console.log(`pixel${l} destroyed`);
+                rowsToDelete.removeChild(pixelsToDelete);
+
+
+            }
+            
+            else
+            {
+                pixelsToDelete = document.querySelector(`#pixel${l + (i-1) * lastCanvasNum}`);
+                
+                console.log(`pixel${l + (i-1) * lastCanvasNum} destroyed`);
+                rowsToDelete.removeChild(pixelsToDelete);
+                
+            }
+            
+
+        }
+        console.log(`row${i} destroyed`);
+        pixelContainer.removeChild(rowsToDelete);
+
+        
+
+    
+    }
+
+    
+
+}
+
+let test = document.querySelector(".test");
+test.addEventListener('click',removeCanvas); 
+
+
